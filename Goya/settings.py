356582dc7a3f3b113b8e6b7167c63cd69267db07
Nestor <ngonzalez@batrescs.com>
2020-10-19 09:15:56 -0600

    email request

--- Goya/settings.py
+++ Goya/settings.py
@@ -134,6 +134,15 @@ STATICFILES_FINDER = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = 'media/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST_USER = 'ngonzalezr@miumg.edu.gt'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = 'N n12345678'
+
+
 LOGIN_URL = '/users/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = LOGIN_URL
