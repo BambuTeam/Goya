d380799bb4bd5ac29067d9284a1c755784138a02
Oscar1720 <oscar1720antonio@gmail.com>
2021-04-06 10:54:44 -0600

    Update

--- Goya/settings.py
+++ Goya/settings.py
@@ -157,12 +157,12 @@ STATICFILES_FINDER = [
 
 STATIC_ROOT = 'static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/'),
 )
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/'),
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
