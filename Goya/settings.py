4e8388ce740961ea2b5aae6e0f856f9a39bcd042
Oscar1720 <oscar1720antonio@gmail.com>
2021-04-06 11:03:17 -0600

    Update

--- Goya/settings.py
+++ Goya/settings.py
@@ -166,7 +166,7 @@ STATICFILES_DIRS = (
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST_USER = 'ngonzalezr@miumg.edu.gt'
