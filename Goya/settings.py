38b8afd418e0a4a8d1074bdfbacfa707d939baf5
Oscar1720 <oscar1720antonio@gmail.com>
2021-04-06 15:31:09 -0600

    Update

--- Goya/settings.py
+++ Goya/settings.py
@@ -157,13 +157,13 @@ STATICFILES_FINDER = [
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static/')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
