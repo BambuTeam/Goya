f19deeafbf2c0225660d919ffe7c9aeb04596614
Oscar1720 <oscar1720antonio@gmail.com>
2021-04-06 15:14:12 -0600

    Update

--- Goya/settings.py
+++ Goya/settings.py
@@ -157,7 +157,7 @@ STATICFILES_FINDER = [
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
